fb22af6937846feb2dca1d2561612b6a0665016a
ranabadawy-02 <ranamohsen1102@gmail.com>
2025-11-01 19:39:21 +0200

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -10,7 +10,7 @@ LEFT JOIN Booking AS b
 GROUP BY u.user_id, u.first_name, u.last_name
 ORDER BY total_bookings DESC;
 
--- 2. Window function: Rank properties based on number of bookings
+-- 2. Window function: Rank properties based on number of bookings using ROW_NUMBER()
 SELECT
     p.property_id,
     p.name AS property_name,
@@ -21,3 +21,15 @@ LEFT JOIN Booking AS b
     ON p.property_id = b.property_id
 GROUP BY p.property_id, p.name
 ORDER BY booking_rank;
+
+-- 2. Window function: Rank properties using RANK()
+SELECT
+    p.property_id,
+    p.name AS property_name,
+    COUNT(b.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS rank_position
+FROM Property AS p
+LEFT JOIN Booking AS b
+    ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name
+ORDER BY rank_position;
